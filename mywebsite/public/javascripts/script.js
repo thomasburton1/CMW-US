a58a8163667fc3030d6361e86f85af3b4396487a
thomasburton1 <thbu3738@colorado.edu>
2021-12-21 21:45:26 -0800

    worked on EV page a bit, moving on to set up basic contact and investor forms for now. Should link directly to executive's email

---
+++ mywebsite/public/javascripts/script.js
@@ -0,0 +1,19 @@
+/*$('#company-link').click(function () {
+    $()
+
+    $('html, body').animate({
+        scrollTop: $('#company-section').offset().top
+    }, 2000);
+});
+
+$('#team-link').click(function () {
+    $('html, body').animate({
+        scrollTop: $('#team-section').offset().top
+    }, 2000);
+});
+
+$('#partners-link').click(function () {
+    $('html, body').animate({
+        scrollTop: $('#partners-section').offset().top
+    }, 2000);
+});*/
